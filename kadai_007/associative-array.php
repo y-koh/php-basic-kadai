73c96f79eb1bb18e8fa68b4f1bab9ab4faf5d6f0
吉川弥生 <tytytyyy2684@gmail.com>
2024-10-08 16:03:37 +0900

    kadai_007を登録

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP連想配列・基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+            $stuff = ['name' => 'onion', 'price' => '200', 'weight' => '160',];
+            print_r($stuff); 
+        ?>
+    </p>
+</body>
+</html>
